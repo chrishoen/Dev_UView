6cbf4fba46c3d59db087108a8e9501d23d4dc6c2
chrishoen <chris123hoen@gmail.com>
2019-03-18 14:25:19 -0400

    TestForm

--- TestForm/MainForm.Designer.cs
+++ TestForm/MainForm.Designer.cs
@@ -40,6 +40,7 @@
          this.button1.TabIndex = 0;
          this.button1.Text = "button1";
          this.button1.UseVisualStyleBackColor = true;
+         this.button1.Click += new System.EventHandler(this.button1_Click);
          // 
          // textBox1
          // 
@@ -47,6 +48,7 @@
          this.textBox1.Name = "textBox1";
          this.textBox1.Size = new System.Drawing.Size(100, 20);
          this.textBox1.TabIndex = 1;
+         this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
          // 
          // MainForm
          // 
