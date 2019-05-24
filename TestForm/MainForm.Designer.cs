9bd85433ec2b1e65e621309623248815cdf77045
chrishoen <chris123hoen@gmail.com>
2019-05-24 16:12:42 -0400

    more

--- TestForm/MainForm.Designer.cs
+++ TestForm/MainForm.Designer.cs
@@ -60,7 +60,7 @@
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
